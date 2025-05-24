1673d56a6f408a3e6c1bfa464ecae55218872c73
itshaiderabro <itshaiderabro@gmail.com>
2025-05-24 17:14:18 +0500

    User

--- item/views.py
+++ item/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def login_view(request):
+    pass 
+
+def logout_view(request):
+    pass 
+
+def register(request):
+    pass
+
